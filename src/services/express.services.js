998f109c13d5c2318631d9dc56af914d7cd7cf67
NicoDev75 <45965028+NicoDev75@users.noreply.github.com>
2020-11-19 12:04:36 +0100

    maj des routes

--- src/services/express.services.js
+++ src/services/express.services.js
@@ -1,12 +1,12 @@
 
 const server = require('../configs/server.configs');
 const bodyParser =  require('body-parser');
-const userRoute = require('../routes/user');
+const userRoute = require('../routes/user.routes.js');
 const express = require('express');
 
 const app = express()
 app.use(bodyParser.json());
-app.use('/user', userRoute);
+app.use('/api/v1', userRoute);
 
 exports.start= () =>{
     port = server.port;
